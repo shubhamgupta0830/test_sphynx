52fc4b8d134efb5fc4e843fed031ef439dbbb6b0
aiona-ai <shubham_gupta@berkeley.edu>
2024-10-28 18:36:23 -0500

    2_trial

--- source/conf.py
+++ source/conf.py
@@ -52,7 +52,19 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_theme_options = {
+    'style_nav_header_background': '#2980B9',
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': True,
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ['custom.css']
